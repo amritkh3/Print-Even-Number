44d094d48bd29fb514989c1f0abae60065e73aaa
Amrit khatri <amritkhatri95@gmail.com>
2022-08-01 14:42:41 -0400

    this is first commit

---
+++ print10evenNumber.py
@@ -0,0 +1,20 @@
+"""print 10 even number.
+1.declare a variable with name i and assign it with value 0
+2.declare another variable with name num and assign it with 1
+3.run while loop till i<11
+4.use if condition if num modulus value with 2 is 0
+5. print num
+6.increment the value of i by 1 each time 
+7. increment hte value of num by 1 each time 
+"""
+# i=0 #(i is the increment)
+num=1
+while num<22:
+    if num%2==0:
+
+
+       print(num)
+      #  i=i+1
+    num=num+1
+
+     
